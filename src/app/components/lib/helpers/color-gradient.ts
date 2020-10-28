17cde6658f9a1f8ebf3a15d035019d9bd3430421
Agustin Pane <agustin.pane@gmail.com>
2020-10-28 17:43:17 -0300

    Adds renderOpacityValuesMap

--- src/app/components/lib/helpers/color-gradient.ts
+++ src/app/components/lib/helpers/color-gradient.ts
@@ -46,3 +46,36 @@ export const renderColorMap = (canvas: HTMLCanvasElement, diameter: number, coef
 
   return canvas;
 };
+
+export const renderOpacityValuesMap = (canvas: HTMLCanvasElement, diameter: number, coefficient: number = 0.77): HTMLCanvasElement => {
+  canvas.width = canvas.height = diameter;
+  const radius = diameter / 2;
+  const toRad = (2 * Math.PI) / 360;
+  const step = 0.5;
+  const aliasing = 1;
+  const lineWidth = 2;
+
+  const INITIAL_ANGLE = -90;
+  const FINAL_ANGLE = 90;
+  const ctx = canvas.getContext('2d');
+  ctx.clearRect(0, 0, diameter, diameter);
+  for (let i = INITIAL_ANGLE; i <= FINAL_ANGLE; i += step) {
+    const startAngle = i > INITIAL_ANGLE ? (i - aliasing) * toRad : i * toRad;
+    const endAngle = (i + step) * toRad;
+    ctx.beginPath();
+    ctx.arc(radius, radius, radius, startAngle, endAngle, false);
+    ctx.strokeStyle = `hsl(0, 100%, ${i}%)`;
+    ctx.lineWidth = radius;
+    ctx.closePath();
+    ctx.stroke();
+  }
+
+  // Replaces circle with white
+  ctx.fillStyle = 'rgb(255, 255, 255)';
+  ctx.beginPath();
+  ctx.arc(radius + lineWidth, radius, radius * coefficient, INITIAL_ANGLE * toRad, FINAL_ANGLE * toRad, false);
+  ctx.closePath();
+  ctx.fill();
+
+  return canvas;
+};
